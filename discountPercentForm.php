0830a9f7abe01e2855d4c3e08329ad8f5e2d444f
Kannika <>
2019-02-01 14:46:27 +0700

    edit1

---
+++ discountPercentForm.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>DiscountPercentForm</title>
+</head>
+<body>
+
+<form action="" method="">
+
+id: <input type="text" name="BookId"> <br>
+Type:
+<select name="typeDiscount">
+  <option value="percent">Percent</option>
+  <option value="Bath">Bath</option>
+</select><br>
+<input type="submit">
+</form>
+</body>
+</html>
